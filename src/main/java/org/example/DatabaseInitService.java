a85ba8823c1f06479bd2ea2522a59f729d0669b7
artur <arturdmytriiev@gmail.com>
2024-09-14 01:44:21 +0200

    Write Java code which can send request to the database using SQL requests

---
+++ src/main/java/org/example/DatabaseInitService.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class DatabaseInitService {
+    private static final String FOLDER_URL = "src/main/resources/sql/init_db.sql";
+    public static void main(String[] args) {
+        try {
+            Connection connection = Database.getInstance().getConnection();
+            List<String> sqlLines = Files.readAllLines(Paths.get(FOLDER_URL));
+            String sqlRequest = String.join("\n", sqlLines);
+            try(Statement statement = connection.createStatement())
+            {
+                statement.execute(sqlRequest);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
